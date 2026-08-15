a84559965aa7d29c7855db56f9f9d7f29b478be4
Gabriel Pagani <gabrielpaganidesouza@gmail.com>
2026-08-15 13:38:50 -0300

    fix: Update database configuration to use environment variables for host and port

--- project/settings.py
+++ project/settings.py
@@ -72,8 +72,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'postgres',
-        'PORT': '5432',
+        'HOST': os.getenv('POSTGRES_HOST', 'postgres'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
