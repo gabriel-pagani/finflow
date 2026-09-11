8e69a1aa95b03fa1a41c31b9e3284a1f2045c21f
Gabriel Pagani <gabrielpaganidesouza@gmail.com>
2026-09-11 12:27:58 -0300

    feat(regras): restringe tipo e método às opções previstas

--- app/models/business_rule.py
+++ app/models/business_rule.py
@@ -14,7 +14,21 @@ class BusinessRule(models.Model):
     class Meta:
         ordering = ['account__description', 'type', 'method']
         constraints = [
-            models.UniqueConstraint(fields=['account', 'type', 'method'], name='business_rule_unique_combination'),
+            models.UniqueConstraint(
+                fields=['account', 'type', 'method'],
+                name='business_rule_unique_combination',
+                violation_error_message='Essa conta já tem uma regra com esse tipo e método.',
+            ),
+            models.CheckConstraint(
+                condition=models.Q(type__in=Type.values),
+                name='business_rule_type_within_choices',
+                violation_error_message='O tipo precisa ser uma das opções previstas.',
+            ),
+            models.CheckConstraint(
+                condition=models.Q(method__in=Method.values),
+                name='business_rule_method_within_choices',
+                violation_error_message='O método precisa ser uma das opções previstas.',
+            ),
         ]
         verbose_name = 'Regra de Negócio'
         verbose_name_plural = 'Regras de Negócio'
--- tests/models/test_business_rule_constraints.py
+++ tests/models/test_business_rule_constraints.py
@@ -1,4 +1,4 @@
-"""Unicidade da trinca conta, tipo e método."""
+"""Unicidade da trinca conta, tipo e método, e tipo e método dentro das opções."""
 import pytest
 from django.db.utils import IntegrityError
 
@@ -18,3 +18,15 @@ def test_mesma_conta_com_outro_metodo_e_aceita(account, credit_rule):
 def test_mesmo_metodo_em_outro_tipo_e_aceito(account, credit_rule):
     BusinessRule.objects.create(account=account, type=Type.IN, method=Method.CREDIT)
     assert BusinessRule.objects.count() == 2
+
+
+@pytest.mark.parametrize('type', ['', 'TRANSFER'])
+def test_tipo_fora_das_opcoes_e_recusado(account, type):
+    with pytest.raises(IntegrityError):
+        BusinessRule.objects.create(account=account, type=type, method=Method.DEBIT)
+
+
+@pytest.mark.parametrize('method', ['', 'PIX'])
+def test_metodo_fora_das_opcoes_e_recusado(account, method):
+    with pytest.raises(IntegrityError):
+        BusinessRule.objects.create(account=account, type=Type.OUT, method=method)
