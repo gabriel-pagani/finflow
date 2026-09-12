c63b680f22d0ea512856a265a860157f9da90533
Gabriel Pagani <gabrielpaganidesouza@gmail.com>
2026-09-12 18:10:47 -0300

    refactor: remove do portal de administração que não devem ser gerenciados pelo portal

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,14 +1,10 @@
-from . import account, business_rule, card, category, group, installment, transaction, transfer, user
+from . import account, business_rule, category, group, user
 
 
 __all__ = [
     'account',
     'business_rule',
-    'card',
     'category',
     'group',
-    'installment',
-    'transaction',
-    'transfer',
     'user',
 ]
